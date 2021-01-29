fda467ffe23e3a52f1dba2b7cecea9d2d36f7bdd
Gino <204410178@gms.tku.edu.tw>
2021-01-29 23:48:33 +0800

    schedule downlink

--- allocate.py
+++ allocate.py
@@ -1,5 +1,6 @@
 import scenario
 import channel
+import tools
 import json
 import numpy as np
 
@@ -14,6 +15,10 @@ class Allocate():
             config = json.load(f)
             f.close()
         
+        self.N_dBm = config["N_dBm"]
+        self.bw = config["bw"]
+        self.N0 = (10**(self.N_dBm / 10))
+        self.N0 = self.N0 * self.bw
         self.numCUE = config["numCUE"]
         self.numD2D = config["numD2D"]
         self.numRB = config["numRB"]
@@ -21,8 +26,13 @@ class Allocate():
         self.radius = config["radius"]
         self.totalBeam = config["totalBeam"]
         self.scheduleBeam = config["scheduleBeam"]
-        self.dataMax = config["dataMax"]
-        self.dataMin = config["dataMin"]
+        self.firPb = config["firPb"]
+        self.secPb = config["secPb"]
+        self.dataUEMax = config["dataUEMax"]
+        self.dataUEMin = config["dataUEMin"]
+
+        self.convert = tools.Convert()
+        self.tool = tools.Tool()
         
         self.schedule = np.zeros((self.totalBeam, 4))
         x = channel.Channel(dis_C2BS, dis_D, dis_C2D, dis_D2C, dis_BS2D, dis_DiDj, dis_D2BS, numD2DReciver)
@@ -66,10 +76,69 @@ class Allocate():
                         self.scheduleUE[cue] = np.where(beamCandicate == beam)[0]
                     else:
                         self.scheduleUE[cue] = np.where(beamCandicate == beam)[0]
+        
+        candicateUE = np.where(self.scheduleUE >= 0)[0]
+        ueData = np.random.randint(low=self.dataUEMin, high=self.dataUEMax, size=len(self.scheduleUE))
+        ueMinSINR_dB = np.zeros(len(self.scheduleUE))
+        ueMinCQI = np.zeros(len(self.scheduleUE))
+        ueMinTbs = np.zeros(len(self.scheduleUE))
+        for i in range(len(self.scheduleUE)):
+            tbs = self.tool.perRB_TBS_mapping(1, ueData[i])
+            cqi = self.convert.TBS_CQI_mapping(tbs)
+            sinr = self.convert.CQI_SINR_mapping(cqi)
+            ueMinCQI[i] = cqi
+            ueMinSINR_dB[i] = sinr
+        
+        ueSNR = np.zeros((self.numCUE, self.numRB))
+        ueSNR_db_rb = np.zeros((self.numCUE, self.numRB))
+        ueSNR_db = np.zeros(self.numCUE)
+        
+        assignRB = np.zeros((self.numCUE, self.numRB))
+        rbStatus = np.zeros(self.numRB)
+        for index in candicateUE:
+            for rb in range(self.numRB):
+                if self.scheduleUE[index] == 0:
+                    snr = self.calculate_SNR(self.firPb, self.g_b[index][rb], self.N0)
+                    snr_db = self.convert.mW_to_dB(snr)
+                    ueSNR[index][rb] = snr
+                    ueSNR_db_rb[index][rb] = snr_db
+
+                elif self.scheduleUE[index] == 1 or self.scheduleUE[index] == 2:
+                    snr = self.calculate_SNR(self.secPb, self.g_b[index][rb], self.N0)
+                    snr_db = self.convert.mW_to_dB(snr)
+                    ueSNR[index][rb] = snr
+                    ueSNR_db_rb[index][rb] = snr_db
+        sortSNR = (-ueSNR_db_rb).argsort(axis=1,)
+
+        for ue in candicateUE:
+            i = 0
+            while i < self.numRB:
+                if rbStatus[sortSNR[ue][i]] == 0:
+                    rbStatus[sortSNR[ue][i]] = 1
+                    assignRB[ue][sortSNR[ue][i]] = 1
+                    break
+                else:
+                    i += 1
+        
+        for i in range(self.numCUE):
+            for j in range(self.numRB):
+                if assignRB[i][j] == 1:
+                    ueSNR_db[i] = ueSNR_db_rb[i][j]
+
         self.scheduleUE = np.full(self.numCUE, -1)
     
     def isInsideSector(self, u, v):
         return -u[0]*v[1] + u[1]*v[0] > 0
+
+    def alloc_uplink(self, c_x, c_y):
+        
+
+    def calculate_SNR(self, uePower, gain, N0):
+        return (uePower * gain) / N0
+        #基地台打Power , CUE可以算SNR然後得到CQI可得TBS Index
+
+
+                
 
 # u = scenario.Genrator()
 # dis_C2BS, dis_D, dis_C2D, dis_D2C, dis_BS2D, dis_DiDj, dis_D2BS, numD2DReciver = u.genrator()
@@ -80,7 +149,7 @@ allocate = Allocate(dis_C2BS, dis_D, dis_C2D, dis_D2C, dis_BS2D, dis_DiDj, dis_D
 # numbeam = 8
 # for i in range(100):
 #     print("{} time with {} beam".format(i, i%numbeam+1))
-for i in range(10):
+for i in range(8):
     allocate.alloc_downlink(i, c_x, c_y)
-
-# x.draw_model()
+# allocate.alloc_downlink(0, c_x, c_y)
+x.draw_model()
---
+++ tools.py
@@ -0,0 +1,155 @@
+import  numpy as np
+import json
+
+class Convert:
+    def mW_to_dB(self, mW):
+        dB = 10*np.log10(mW)
+        return np.round(dB, decimals=0)
+    
+    def dB_to_mW(self, dB):
+        mW = 10**(dB/10)
+        return np.round(mW, decimals=0)
+    
+    def CQI_SINR_mapping(slef, CQI):
+        if CQI == 1:
+            return -6.7
+        elif CQI == 2:
+            return -4.7
+        elif CQI == 3:
+            return -2.3
+        elif CQI == 4:
+            return 0.2
+        elif CQI == 5:
+            return 2.4
+        elif CQI == 6:
+            return 4.3
+        elif CQI == 7:
+            return 5.9
+        elif CQI == 8:
+            return 8.1
+        elif CQI == 9:
+            return 10.3
+        elif CQI == 10:
+            return 11.7
+        elif CQI == 11:
+            return 14.1
+        elif CQI == 12:
+            return 16.3
+        elif CQI == 13:
+            return 18.7
+        elif CQI == 14:
+            return 21.0
+        elif CQI == 15:
+            return 22.7
+    
+    def SINR_CQI_mapping(self,sinr):
+        if sinr >= -6.7 and sinr < -4.7:
+            return 1
+        elif sinr >= -4.7 and sinr < -2.3:
+            return 2
+        elif sinr >= -2.3 and sinr < 0.2:
+            return 3
+        elif sinr >= 0.2 and sinr < 2.4:
+            return 4
+        elif sinr >= 2.4 and sinr < 4.3:
+            return 5
+        elif sinr >= 4.3 and sinr < 5.9:
+            return 6
+        elif sinr >= 5.9 and sinr < 8.1:
+            return 7
+        elif sinr >= 8.1 and sinr < 10.3:
+            return 8
+        elif sinr >= 10.3 and sinr < 11.7:
+            return 9
+        elif sinr>= 11.7 and sinr < 14.1:
+            return 10
+        elif sinr >= 14.1 and sinr < 16.3:
+            return 11
+        elif sinr >= 16.3 and sinr < 18.7:
+            return 12
+        elif sinr >= 18.7 and sinr < 21.0:
+            return 13
+        elif sinr >= 21.0 and sinr < 22.7:
+            return 14
+        elif sinr >= 22.7:
+            return 15
+    
+    def CQI_MCS_mapping(self, CQI):
+        Qm = 0
+        if CQI >= 1 and CQI <=6:
+            Qm = 2
+        elif CQI >= 7 and CQI <= 9:
+            Qm = 4
+        elif CQI >= 10 and CQI <= 15:
+            Qm = 6
+        return Qm
+
+    def MCS_TBS_mapping(self, MCS):
+        if MCS == 2:
+            return np.random.randint(0,9)
+        elif MCS == 4:
+            return np.random.randint(9,15)
+        elif MCS == 6:
+            return np.random.randint(15,26)
+    
+    def TBS_CQI_mapping(self, tbs):
+        CQI = 0
+        if tbs ==0 or tbs == 1:
+            CQI = 1
+        elif tbs == 2 or tbs == 3:
+            CQI = 2
+        elif tbs == 4 or tbs == 5:
+            CQI = 3
+        elif tbs == 6 or tbs == 7:
+            CQI = 4
+        elif tbs == 8:
+            CQI = 5
+        elif tbs == 9 or tbs == 10 or tbs == 11:
+            CQI = np.random.randint(low=5, high=7)
+        elif tbs == 12 or tbs == 3:
+            CQI = 8
+        elif tbs == 14:
+            CQI = 9
+        elif tbs == 15 or tbs == 16:
+            CQI = np.random.randint(low=9, high=10)
+        elif tbs == 17 or tbs == 18:
+            CQI = 11
+        elif tbs == 19 or tbs == 20:
+            CQI = 12
+        elif tbs == 21 or tbs == 22:
+            CQI = 13
+        elif tbs == 23 or tbs == 24:
+            CQI = 14
+        elif tbs == 25 or tbs == 26:
+            CQI = 15
+        return CQI
+
+class Tool:
+    def TBS(self):
+        with open("Throughput.json", "r") as f:
+            tbs = json.load(f)
+            f.close()
+        return tbs
+
+    def Throughput(self, index, data):
+        tbs = self.TBS()
+        rb = 0
+        for throughput in tbs[str(index)]:
+            if data > throughput:
+                rb += 1
+        return rb+1
+    
+    def perRB_TBS_mapping(self, numRB, data):
+        tbs = self.TBS()
+        tbs_index = 0
+        for index in tbs:
+            if index == '26A':
+                break
+            if tbs[index][numRB-1] >= data:
+                tbs_index = index
+                break
+        return int(tbs_index)
+
+    def tbs_rb__throughput(self, index, rb):
+        tbs = self.TBS()
+        throughput = tbs[str(index)]
