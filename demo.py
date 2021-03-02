7c14dc79345800a4d06b7abaf3bba9959972775a
Gino <204410178@gms.tku.edu.tw>
2021-03-02 10:17:02 +0800

    update demo

--- demo.py
+++ demo.py
@@ -16,93 +16,82 @@ bw = 180e3
 N0 = (10**(N_dBm / 10)) * 10**(-3) #1Hz的熱噪聲，單位mW
 N0 = N0 * bw  
 
-g = genrator.Genrator(250)
+g = genrator.Genrator(30)
 c = channel.Channel(numRB, numD2DReciver)
 
 txPoint = g.generateTxPoint(numD2D+1)
 rxPoint = g.generateRxPoint(txPoint, D2Ddistance, numD2DReciver)
+
+print(txPoint)
+print(rxPoint)
+
+# txPoint = [
+#     [ -36.45178991, 240.85655738],
+#     [-220.22166836, -9.79381262],
+#     [ 224.14173945, -87.99891978],
+#     [ 103.60784244, 191.35542775]
+# ]
+# rxPoint = [
+#     [[ -44.37468351, 201.47684126]],
+#     [[-193.29184759,  17.51893835]],
+#     [[ 207.41464108, -128.13631782]],
+#     [[  64.34542723, 183.224726  ]]
+# ]
 
 distance = g.distanceD2DRx(txPoint, rxPoint, numD2DReciver)
 d_d2d = g.distanceTx2D2DRx(txPoint, rxPoint, numD2DReciver)
 
 g_d = c.gainD2DRx(distance)
 g_d2d = c.gainTx2D2DRx(d_d2d)
+# print(d_d2d*1000)
+print(g_d)
+print(g_d2d)
 
-g_d = [
-    [[2.52654070e-07, 7.86123808e-07]],
-    [[1.43375716e-08, 1.15423152e-08]],
-    [[1.50439730e-07, 1.20634622e-07]],
-    [[4.85914641e-08, 1.68893913e-08]]
-]
 
-g_d2d = [
-    [
-        [[2.52654070e-07, 7.86123808e-07]],
-        [[6.57438677e-13, 1.00925710e-10]],
-        [[7.22180835e-10, 4.50687393e-10]],
-        [[6.12916282e-14, 8.39837051e-11]]
-    ],
+# g_d = [
+#     [[2.52654070e-07, 7.86123808e-07]],
+#     [[1.43375716e-08, 1.15423152e-08]],
+#     [[1.50439730e-07, 1.20634622e-07]],
+#     [[4.85914641e-08, 1.68893913e-08]]
+# ]
 
-    [
-        [[7.74513601e-11, 4.80539163e-11]],
-        [[1.43375716e-08, 1.15423152e-08]],
-        [[6.55443400e-11, 1.26268250e-10]],
-        [[2.16496453e-08, 4.20074591e-09]]
-    ],
+# g_d2d = [
+#     [
+#         [[2.52654070e-07, 7.86123808e-07]],
+#         [[6.57438677e-13, 1.00925710e-10]],
+#         [[7.22180835e-10, 4.50687393e-10]],
+#         [[6.12916282e-14, 8.39837051e-11]]
+#     ],
 
-    [
-        [[1.09471417e-10, 7.49686906e-10]],
-        [[2.34229552e-10, 1.58610780e-09]],
-        [[1.50439730e-07, 1.20634622e-07]],
-        [[1.25396661e-11, 1.16581788e-10]]
-    ],
+#     [
+#         [[7.74513601e-11, 4.80539163e-11]],
+#         [[1.43375716e-08, 1.15423152e-08]],
+#         [[6.55443400e-11, 1.26268250e-10]],
+#         [[2.16496453e-08, 4.20074591e-09]]
+#     ],
 
-    [
-        [[7.57590311e-11, 3.12787270e-11]],
-        [[3.69387130e-09, 2.21505918e-09]],
-        [[6.09386531e-10, 3.19896926e-10]],
-        [[4.85914641e-08, 1.68893913e-08]]
-    ]
-]
+#     [
+#         [[1.09471417e-10, 7.49686906e-10]],
+#         [[2.34229552e-10, 1.58610780e-09]],
+#         [[1.50439730e-07, 1.20634622e-07]],
+#         [[1.25396661e-11, 1.16581788e-10]]
+#     ],
 
-powerList = [-22, 3.3, -4.8]
+#     [
+#         [[7.57590311e-11, 3.12787270e-11]],
+#         [[3.69387130e-09, 2.21505918e-09]],
+#         [[6.09386531e-10, 3.19896926e-10]],
+#         [[4.85914641e-08, 1.68893913e-08]]
+#     ]
+# ]
+
+powerList = [-22, 3.3, -20]
 powerList_mW = [0, 0, 0]
 for i in range(numD2D):
     powerList_mW[i] = convert.dB_to_mW(powerList[i]) / 1000
 
 data = [440, 336, 100]
 background = convert.dB_to_mW(-5) / 1000
-
-# for j in range(numD2D-1):
-#     tbs, rb = tool.data_tbs_mapping(data[j], 1)
-#     cqi = convert.TBS_CQI_mapping(tbs)
-#     sinr = convert.CQI_SINR_mapping(cqi)
-#     minSinr = convert.dB_to_mW(sinr)
-#     i = background * g_d2d[3][j][0][0]
-#     power = (minSinr * (N0 + i)) / g_d[j][0][0]
-#     powerDB = convert.mW_to_dB(power*1000)
-
-#     print('minSINR',sinr)
-#     print('minPower',powerDB)
-#     s = (power * g_d[j][0][0]) / (N0 + i)
-#     print('sinr',convert.mW_to_dB(s))
-#     print()
-
-# tbs, rb = tool.data_tbs_mapping(data[2], 1)
-# cqi = convert.TBS_CQI_mapping(tbs)
-# sinr = convert.CQI_SINR_mapping(cqi)
-# minSinr = convert.dB_to_mW(sinr)
-# i = background * g_d
-# power = (minSinr * (N0 + i)) / g_d
-
-# print(power)
-# powerDB = convert.mW_to_dB(power*1000)
-# print(powerDB)
-# s = (power * g_d) / (N0 + i)
-# print(convert.mW_to_dB(s))
-# print()
-
-
 
 for d2d in range(numD2D-1):
     tbs, rb = tool.data_tbs_mapping(data[d2d], 1)
@@ -119,6 +108,7 @@ for d2d in range(numD2D-1):
     i3 = powerList_mW[2] * g_d2d[2][d2d][0][0] + background * g_d2d[3][d2d][0][0]
     iSINR = (powerList_mW[d2d] * g_d[d2d][0][0]) / (N0 + i3)
     iSINR_DB = convert.mW_to_dB(iSINR)
+    print('power {} x gain {} / N0 {} + i {} = sinr {}'.format(powerList_mW[d2d], g_d[d2d][0][0], N0, i3, (powerList_mW[d2d] * g_d[d2d][0][0]) / (N0 + i3)))
     print('If d2d 3 use this RB, d2d',d2d,'sinr could be',iSINR_DB)
     print()
 
@@ -126,6 +116,7 @@ for d2d in range(numD2D-1):
 
 print()
 tbs, rb = tool.data_tbs_mapping(data[0], 2)
+print(tbs)
 cqi = convert.TBS_CQI_mapping(tbs)
 sinr = convert.CQI_SINR_mapping(cqi)
 minSinr = convert.dB_to_mW(sinr)
@@ -136,6 +127,8 @@ print('minSINR',sinr)
 SINR = (powerList_mW[0] * g_d[0][0][0]) / (N0 + i)
 SINR_DB = convert.mW_to_dB(SINR)
 print('sinr',SINR_DB)
+print('{} * {} + {} * {} + {} * {} = {}'.format(powerList_mW[1], g_d2d[1][0][0][0], powerList_mW[2], g_d2d[2][0][0][0], background, g_d2d[3][0][0][0], i))
+print('{} * {} / ({} + {}) = {}'.format(powerList_mW[0], g_d[0][0][0], N0, i, (powerList_mW[0] * g_d[0][0][0]) / (N0 + i)))
 print()
 
 print()
