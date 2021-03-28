0a193861c387e0c7c65a356fbd0effb27a49d6c4
HE02737964 <37354737+HE02737964@users.noreply.github.com>
2021-03-29 04:06:53 +0800

    add some fun 03/29

--- method.py
+++ method.py
@@ -16,11 +16,11 @@ def phase1(**parameter):
         if parameter['powerD2DList'][root] == 0 and root not in parameter['nStartD2D']:
             longestPath = find_longest_path(root, **parameter)
             
-            if longestPath:
-                parameter['powerD2DList'][longestPath[-1]] = 1
+            if parameter['powerD2DList'][3] == 0:
+                parameter['powerD2DList'][3] = 1
         # print(parameter['powerD2DList'])
         # print(longestPath)
-        # print()
+        print()
     
     return parameter
 
@@ -36,6 +36,7 @@ def cal_num_interfered_neighbor(**parameter):
 #計算D2D的priority(越大優先權越高)
 def cal_priority(**parameter):
     priority = (parameter['data_d2d'] / parameter['num_interference']) * (1 / parameter['scheduleTimes'])
+    priority = np.asarray([1,5,2,3,4])
     sort_priority = (-priority).argsort()
     parameter.update({'priority' : priority})
     parameter.update({'priority_sort_index' : sort_priority})
@@ -74,11 +75,12 @@ def find_longest_path(root, **parameter):
     longestPath = []
     power_assign_list = np.where(parameter['powerD2DList'] != 0)[0]
     not_visit_point = np.concatenate((power_assign_list, parameter['nStartD2D']))
-    print(not_visit_point)
     vis = [False] * len(parameter['d2d_no_cell_interference_graph'])
     if not vis[root]:
         path = []
         longestPath = dfs(root, parameter['d2d_no_cell_interference_graph'], not_visit_point, vis, path, longestPath)
+    #所有的path做排序
+    # longestPath = sorted(longestPath, key = len, reverse=True)
     print(longestPath)
     if parameter['longestPath_type'] == "priority":
         return longestPath[0]
@@ -100,3 +102,33 @@ def dfs(node, graph, not_visit_point, vis, path, longestPath):
         vis[node] = False
 
     return longestPath
+
+#得到d2d能使用的rb
+def get_d2d_use_rb(d2d, **parameter):
+    d2dUseRBList = np.ones(25, dtype=int)
+    cueUseRBList = np.zeros(25, dtype=int)
+    for cue in range(parameter['numCellRx']):
+        if d2d in parameter['i_d2c'][cue]:
+            #CUE有使用的RB做or運算，找出所有rx cue會被d2d干擾的RB
+            cueUseRBList = np.logical_or(cueUseRBList, parameter['assignmentRxCell'][cue])
+    d2dUseRBList = d2dUseRBList - cueUseRBList
+    return d2dUseRBList
+
+#計算d2d每個rx在每個rb上的干擾
+def cal_d2d_interference(tx, rx, rb, **parameter):
+    interference = 0
+    for i in parameter['i_d2d_rx'][tx][rx]['d2d']:
+        if parameter['assignmentD2D'][tx][rb] == 1 and parameter['assignmentD2D'][i][rb] == 1:
+            interference = interference + (parameter['powerD2DList'][i] * parameter['g_dij'][i][tx][rx][rb])
+    for i in parameter['i_d2d_rx'][tx][rx]['cue']:
+        if parameter['assignmentD2D'][tx][rb] == 1 and parameter['assignmentTxCell'][i][rb] == 1:
+            interference = interference + (parameter['powerCUEList'][i] * parameter['g_c2d'][i][tx][rx][rb])
+    return interference
+
+#計算cue在每個rb上的干擾
+def cal_cue_interference(cue, rb, **parameter):
+    interference = 0
+    for i in parameter['i_d2c'][cue]:
+        if parameter['assignmentRxCell'][cue][rb] == 1 and parameter['assignmentD2D'][i][rb] == 1:
+            interference = interference + (parameter['powerD2DList'][i] * parameter['g_d2c'][i][cue][rb])
+    return interference
