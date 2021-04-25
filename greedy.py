784a028e1e86a2adf5ec8e164dc051625d8c0956
HE02737964 <37354737+HE02737964@users.noreply.github.com>
2021-04-26 04:06:57 +0800

    greedy 04/26

--- greedy.py
+++ greedy.py
@@ -33,10 +33,18 @@ def cal_priority(**parameter):
 def get_d2d_use_rb(d2d, **parameter):
     d2dUseRBList = np.ones(parameter['numRB'], dtype=int)
     cueUseRBList = np.zeros(parameter['numRB'], dtype=int)
-    for cue in range(parameter['numCellRx']):
-        if d2d in parameter['i_d2c'][cue]:
-            #CUE有使用的RB做or運算，找出所有rx cue會被d2d干擾的RB
-            cueUseRBList = np.logical_or(cueUseRBList, parameter['assignmentRxCell'][cue])
+    #cue干擾d2d
+    for c_tx in range(parameter['numCellTx']):
+        if d2d in parameter['t_c2d'][c_tx]:
+            cueUseRBList = np.logical_or(cueUseRBList, parameter['assignmentTxCell'][c_tx])
+    #d2d干擾cue
+    for c_rx in range(parameter['numCellRx']):
+        if c_rx in parameter['t_d2c'][d2d]:
+            cueUseRBList = np.logical_or(cueUseRBList, parameter['assignmentRxCell'][c_rx])
+    
+    for d in range(parameter['numD2D']):
+        if d in parameter['t_d2c'][d2d] or d2d in parameter['t_c2d'][d]:
+            d2dUseRBList = np.logical_or(d2dUseRBList, parameter['assignmentD2D'][d])
     d2dUseRBList = d2dUseRBList - cueUseRBList
     parameter['d2d_use_rb_List'][d2d] = d2dUseRBList
     return parameter
@@ -52,16 +60,25 @@ def greedy(**parameter):
     tool = tools.Tool()
     parameter = initial_parameter(**parameter)
 
-    d2d_use_rb_list = []
+    d2d_need_rb = []
 
     #找出d2d所需RB數量、SINR、Power
     for d2d in range(parameter['numD2D']):
-        gain = np.min(parameter['g_d2d'][d2d])
+        parameter = get_d2d_use_rb(d2d, **parameter)
+        numRB = np.sum(parameter['d2d_use_rb_list'][d2d])
         tbs, rb = tool.data_tbs_mapping(parameter['data_d2d'][d2d], parameter['numRB'])
-        cqi, sinr = get_d2d_sys_info(tbs)
-        d2d_use_rb_list.append(rb)
-        parameter['minD2Dsinr'][d2d] = sinr
-        parameter['powerD2DList'][d2d] = ((sinr * parameter['N0']) / gain)
+        d2d_need_rb.append(rb)
+        if numRB == 0 or numRB < d2d_need_rb[d2d]:
+            parameter['nStartD2D'].append(d2d)
+        else:
+            cqi, sinr = get_d2d_sys_info(tbs)
+            parameter['minD2Dsinr'][d2d] = sinr
 
-    # for candicate in parameter['priority_sort_index']:
-        
+    for candicate in parameter['priority_sort_index']:
+        if candicate not in parameter['nStartD2D']:
+            parameter['assignmentD2D'][candicate] = parameter['d2d_use_rb_list'][candicate].copy()
+            gain = np.min(parameter['g_d2d'][d2d])
+            parameter['powerD2DList'][d2d] = ((parameter['minD2Dsinr'][candicate] * parameter['N0']) / gain)
+    print(parameter['t_c2d'])
+    print(parameter['t_d2c'])
+    print(parameter['t_d2d'])
